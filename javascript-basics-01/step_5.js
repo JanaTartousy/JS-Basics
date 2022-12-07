4ae98248c0a494b174a1883f9b3d881778f15fb0
JanaTartousy <tartousyjana@gmail.com>
2022-12-07 20:46:35 +0200

    step_5

--- javascript-basics-01/step_5.js
+++ javascript-basics-01/step_5.js
@@ -1,3 +1,10 @@
-var n1= prompt("ask for number");
-var n2= prompt("ask for number");
-(n1 * n2);
+let x =prompt("enter your first number")
+let first_number = document.getElementById("first_number")
+first_number.value=x
+
+let y =prompt("enter your second number")
+let second_number = document.getElementById("second_number")
+second_number.value=y
+let validate_button =document.getElementById("validate").onclick=function (){
+    alert( x* y)
+}
