f19aea629df0127eb3daeb39c12e42a2474164c0
JanaTartousy <tartousyjana@gmail.com>
2022-12-08 21:41:29 +0200

    sstep_4

---
+++ javascript-basics-03/step 4/sstep_4.js
@@ -0,0 +1,11 @@
+let pass = document.getElementById("#password")
+let confirm = document.getElementById("#confirmation")
+let button = document.querySelector("button")
+
+button.addEventListener("click", (e)=>{
+    if(password.value = confirmation.value) {
+        let border = "1px solid red";
+        password.style.border = border;
+        confirmation.style.border = border;
+    }
+})
