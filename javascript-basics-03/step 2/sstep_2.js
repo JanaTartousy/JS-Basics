5daf447d83b520f6a24fe2e48366fd6f38b4749d
JanaTartousy <tartousyjana@gmail.com>
2022-12-08 19:57:01 +0200

    sstep_2

---
+++ javascript-basics-03/step 2/sstep_2.js
@@ -0,0 +1,12 @@
+var showOrhide = document.querySelectorAll("a")
+var p = document.querySelector("p")
+
+showOrhide .forEach((event)=> {
+    event.addEventListener("click", (event)=> {
+        if(event.target.id == "show") {
+            p.style = `display: block`
+        } else {
+            p.style = `display: none`
+        }
+    })
+})
